fcf9cab065bbdb27f005a9b585f90f10fd3c9c7f
bkajj <muuciek.new@gmail.com>
2025-05-09 22:40:05 +0200

    Add zad8 demo

---
+++ zadanie8/backend/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    kotlin("jvm") version "2.1.10"
+    kotlin("plugin.serialization") version "1.9.0"
+    id("org.jlleitschuh.gradle.ktlint") version "12.2.0"
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+val serializationVersion = "1.6.3"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$serializationVersion")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(23)
+}
