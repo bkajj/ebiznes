63775f71e5579421273df44e2b1d775d11c36963
bkajj <muuciek.new@gmail.com>
2025-05-09 19:18:25 +0200

    Add zadanie7 base

---
+++ zadanie7/frontend/src/components/Cart.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import '../styles/Cart.css';
+import { useNavigate } from 'react-router-dom'; 
+
+const Cart = ({ cart, clearCart }) => {
+  const navigate = useNavigate();
+  const calculateTotal = () => {
+    return cart.reduce((total, item) => total + item.product.price * item.quantity, 0);
+  };
+
+  const handleCheckout = () => {
+    navigate("/payment", { state: { cart } });
+  };
+
+  return (
+    <div className="cart-container">
+      <h1>Koszyk</h1>
+      {cart.length === 0 ? (
+        <p>Twój koszyk jest pusty</p>
+      ) : (
+        <div>
+          <ul>
+            {cart.map((item, index) => (
+              <li key={index}>
+                <div className="product-info">
+                  <span className="product-name">{item.product.name}</span>
+                  <span className="product-quantity">Ilość: {item.quantity}</span>
+                  <span className="product-price">{item.product.price} zł</span>
+                </div>
+              </li>
+            ))}
+          </ul>
+          <div className="total-panel">
+            <div className="total-price">Łączna cena: {calculateTotal()} zł</div>
+            <button className="order-button" onClick={handleCheckout} disabled={cart.length === 0}>Zamów</button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Cart;
