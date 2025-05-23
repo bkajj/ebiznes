7d258a4b64ba84965b23e14aed53a6d2c9487385
bkajj <muuciek.new@gmail.com>
2025-05-23 17:01:19 +0200

    fix

--- zadanie9/chatbot/settings.py
+++ zadanie9/chatbot/settings.py
@@ -11,16 +11,18 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+dotenv.load_dotenv()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'rj#_@(cfpn*mt!-8rp+9zc@zq@$he7obus15dh+@jo=!uq9(+f'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
